1deac84b6331906938074538f1a1a06c9165134d
유지우 <yy7826w@gmail.com>
2025-03-21 13:43:57 +0900

    토스트 추가

---
+++ lib/core/utils/toast.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:lotura/core/component/lotura_toast_widget.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
+enum ToastType {
+  success(Symbols.check_circle_rounded),
+  failure(Symbols.error_rounded);
+
+  Color themeColorHandler(BuildContext context) {
+    switch (this) {
+      case ToastType.success:
+        return Theme.of(context).colorScheme.primary;
+      case ToastType.failure:
+        return Theme.of(context).colorScheme.error;
+    }
+  }
+
+  const ToastType(this.icon);
+
+  final IconData icon;
+}
+
+class ToastUtil {
+  static void toast({
+    required BuildContext context,
+    required String text,
+    required ToastType type,
+  }) async {
+    OverlayEntry overlay = OverlayEntry(
+      builder: (_) => LoturaToastWidget(text: text, type: type),
+    );
+
+    Navigator.of(context).overlay!.insert(overlay);
+
+    await Future.delayed(const Duration(milliseconds: 1500));
+    overlay.remove();
+  }
+}
