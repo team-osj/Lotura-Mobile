72a6090819265030474b2bf32c84419c3b327cab
유지우 <yy7826w@gmail.com>
2025-06-30 23:29:16 +0900

    기기 배치 더미 데이터 추가

---
+++ lib/core/dummy/laundry_status_locate_dummy.dart
@@ -0,0 +1,59 @@
+import 'package:lotura/core/type/device_arrange_type.dart';
+
+List<Map<DeviceArrangeType, List<int>>> maleSchoolLocateDummy = [
+  {
+    DeviceArrangeType.onlyDryer: [33, 1, 9, 41]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [2, 10]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [3, 11]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [4, 12]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [5, 13]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [6, 14]
+  },
+  {
+    DeviceArrangeType.leftWasherRightDryer: [7, 15, 47]
+  },
+  {
+    DeviceArrangeType.leftWasherRightDryer: [8, 16, 48]
+  },
+];
+
+List<Map<DeviceArrangeType, List<int>>> maleDormitoryLocateDummy = [
+  {
+    DeviceArrangeType.onlyDryer: [49, 17, 25, 57]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [18, 26]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [19, 27]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [20, 28]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [21, 29]
+  },
+  {
+    DeviceArrangeType.onlyWasher: [22, 30]
+  },
+  {
+    DeviceArrangeType.onlyDryer: [55, 23, 31, 63]
+  },
+  {
+    DeviceArrangeType.leftDryer: [56, 24]
+  },
+];
+
+List<Map<DeviceArrangeType, List<int>>> femaleDormitoryLocateDummy = [
+
+];
