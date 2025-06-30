5b894d6ae50c143c54cced3f90a76fb737277ade
유지우 <yy7826w@gmail.com>
2025-06-30 23:21:49 +0900

    Device 모델 구현

---
+++ lib/models/device.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:lotura/core/type/device_status_type.dart';
+import 'package:lotura/core/type/device_type.dart';
+import 'package:lotura/core/type/locate_type.dart';
+
+part '../generated/models/device.freezed.dart';
+part '../generated/models/device.g.dart';
+
+@freezed
+class DeviceResponse with _$DeviceResponse {
+  const factory DeviceResponse({
+    required int id,
+    @JsonKey(name: 'state')
+    required DeviceStatusType status,
+    required DeviceType deviceType,
+    required LocateType roomType,
+  }) = _DeviceResponse;
+
+  factory DeviceResponse.fromJson(Map<String, dynamic> json) =>
+      _$DeviceResponseFromJson(json);
+}
