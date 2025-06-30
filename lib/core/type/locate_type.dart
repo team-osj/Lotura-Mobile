78dd5bc1d4b7541b5c79caab7d1cad7ff7903b3b
유지우 <yy7826w@gmail.com>
2025-06-30 23:30:21 +0900

    세탁실 위치 타입에 기기 배열 더미 데이터 getter 추가

--- lib/core/type/locate_type.dart
+++ lib/core/type/locate_type.dart
@@ -1,3 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:lotura/core/dummy/laundry_status_locate_dummy.dart';
+import 'package:lotura/core/type/device_arrange_type.dart';
+
+@JsonEnum(fieldRename: FieldRename.snake)
 enum LocateType {
   maleSchool(text: '남자 학교측', title: '남자 학교 측'),
   maleDormitory(text: '남자 기숙사측', title: '남자 기숙사 측'),
@@ -12,4 +17,15 @@ enum LocateType {
   final String text;
   final String title;
   final String? exception;
+
+  List<Map<DeviceArrangeType, List<int>>> get locateArrange {
+    switch(this) {
+      case LocateType.maleSchool:
+        return maleSchoolLocateDummy;
+      case LocateType.maleDormitory:
+        return maleDormitoryLocateDummy;
+      case LocateType.female:
+        return femaleDormitoryLocateDummy;
+    }
+  }
 }
