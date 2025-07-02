aa68568a762a2c65995bbab7f48bec626ffc33d9
유지우 <yy7826w@gmail.com>
2025-07-02 19:47:07 +0900

    웹소켓 통신 Provider 구현

---
+++ lib/generated/providers/websocket.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../../providers/websocket.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$webSocketManagerHash() => r'03903114c155af8faf24dbaa31108b7908dd4da7';
+
+/// See also [WebSocketManager].
+@ProviderFor(WebSocketManager)
+final webSocketManagerProvider =
+    StreamNotifierProvider<WebSocketManager, DeviceResponse>.internal(
+  WebSocketManager.new,
+  name: r'webSocketManagerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$webSocketManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$WebSocketManager = StreamNotifier<DeviceResponse>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
