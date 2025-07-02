300cd41b4e27ec11ecc042c95bb1c82867ed52d9
유지우 <yy7826w@gmail.com>
2025-07-02 19:46:55 +0900

    Device 목록을 불러오는 Provider 추가

---
+++ lib/generated/providers/device.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../../providers/device.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$deviceManagerHash() => r'd0b2dd4c15195ea99a06c2f68f75766a366ca391';
+
+/// See also [DeviceManager].
+@ProviderFor(DeviceManager)
+final deviceManagerProvider =
+    AsyncNotifierProvider<DeviceManager, List<DeviceResponse>>.internal(
+  DeviceManager.new,
+  name: r'deviceManagerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$deviceManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DeviceManager = AsyncNotifier<List<DeviceResponse>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
