d89f6f669c2543a7221877c7eada458bd39659f7
유지우 <yy7826w@gmail.com>
2025-07-03 23:21:09 +0900

    DeviceStatusType에 속성 추가
    +) displayText, title, caption

--- lib/core/type/device_status_type.dart
+++ lib/core/type/device_status_type.dart
@@ -3,23 +3,62 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 enum DeviceStatusType {
   @JsonValue(0)
-  working('작동 중'),
+  working,
 
   @JsonValue(1)
-  available('사용 가능'),
+  available,
 
   @JsonValue(2)
-  disconnect('연결 끊김'),
+  disconnect,
 
   @JsonValue(3)
-  breakdown('고장');
+  breakdown;
+
+  String get displayText {
+    switch (this) {
+      case DeviceStatusType.working:
+        return '작동 중';
+      case DeviceStatusType.available:
+        return '사용 가능';
+      case DeviceStatusType.disconnect:
+        return '연결 끊김';
+      case DeviceStatusType.breakdown:
+        return '고장';
+    }
+  }
+
+  String title(int id, String device) {
+    switch (this) {
+      case DeviceStatusType.working:
+        return '$id번 $device를\n알림 설정할까요?';
+      case DeviceStatusType.available:
+        return '$id번 $device는\n현재 사용할 수 있어요!';
+      case DeviceStatusType.disconnect:
+        return '$id번 $device는\n연결이 끊겨서 사용할 수 없어요.';
+      case DeviceStatusType.breakdown:
+        return '$id번 $device는\n고장으로 인해 사용할 수 없어요.';
+    }
+  }
+
+  String? caption(String device) {
+    switch (this) {
+      case DeviceStatusType.working:
+        return '$device가 종료되면 알림을 드릴게요.';
+      case DeviceStatusType.available:
+        return null;
+      case DeviceStatusType.disconnect:
+        return '다른 $device를 사용해주세요.\n빠른 시일 내에 수리해 사용 가능하도록 하겠습니다.';
+      case DeviceStatusType.breakdown:
+        return '다른 $device를 사용해주세요.\n빠른 시일 내에 수리해 사용 가능하도록 하겠습니다.';
+    }
+  }
 
   Color themeColorHandler(BuildContext context) {
     switch (this) {
       case DeviceStatusType.working:
-        return Theme.of(context).colorScheme.onTertiary;
-      case DeviceStatusType.available:
         return Theme.of(context).colorScheme.primaryContainer;
+      case DeviceStatusType.available:
+        return Theme.of(context).colorScheme.onTertiary;
       case DeviceStatusType.disconnect:
         return Theme.of(context).colorScheme.secondary;
       case DeviceStatusType.breakdown:
@@ -30,17 +69,13 @@ enum DeviceStatusType {
   Color themeIconColorHandler(BuildContext context) {
     switch (this) {
       case DeviceStatusType.working:
-        return Theme.of(context).colorScheme.tertiary;
-      case DeviceStatusType.available:
         return Theme.of(context).colorScheme.primary;
+      case DeviceStatusType.available:
+        return Theme.of(context).colorScheme.tertiary;
       case DeviceStatusType.disconnect:
         return Theme.of(context).colorScheme.onSecondary;
       case DeviceStatusType.breakdown:
         return Theme.of(context).colorScheme.error;
     }
   }
-
-  const DeviceStatusType(this.text);
-
-  final String text;
 }
