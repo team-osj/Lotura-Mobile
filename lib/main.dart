e636fcafa36e62129be9409d738d0b44ff05dbb1
유지우 <yy7826w@gmail.com>
2025-07-04 22:28:39 +0900

    Firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotura/core/core.dart';
+import 'package:lotura/firebase_options.dart';
 import 'package:lotura/providers/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
