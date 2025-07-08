2e9e719f675c2ad97aefbbc91a36d5769e656758
유지우 <yy7826w@gmail.com>
2025-07-08 14:09:26 +0900

    FCM 설정 (#17)
    * 주석 추가
    
    * Firebase init
    
    * firebase setting
    
    * firebase setting files
    
    * fcm init 추가
    
    * 엄.. 잘못 넣은거 수정
    
    * Local Notification 추가
    
    * Local Notifications iOS 설정 파일
    
    * iOS Minimum Deploy Version 업데이트
    
    * FCM iOS 설정
    
    * Local Notification 라이브러리 제거
    
    * Firebase 설정 완료!

---
+++ lib/core/utils/fcm.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> fcmInit() async {
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: true,
+    sound: true,
+  );
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
